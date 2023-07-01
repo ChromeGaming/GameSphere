aec1a69b59d88fc9e3041c0769dca36eae04caeb
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-07-01 15:20:33 +0530

    Prettier Checked

--- gamenews/script.js
+++ gamenews/script.js
@@ -1,13 +1,13 @@
-const cardsContainer = document.getElementById('cards-container');
+const cardsContainer = document.getElementById("cards-container");
 
 async function fetchDataFromRapidAPI() {
   const url =
-    'https://videogames-news2.p.rapidapi.com/videogames_news/search_news?query=GTA';
+    "https://videogames-news2.p.rapidapi.com/videogames_news/search_news?query=GTA";
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': 'f97024702bmshb016fffd40e7facp1da35ajsnb56b28aa0bdd',
-      'X-RapidAPI-Host': 'videogames-news2.p.rapidapi.com',
+      "X-RapidAPI-Key": "f97024702bmshb016fffd40e7facp1da35ajsnb56b28aa0bdd",
+      "X-RapidAPI-Host": "videogames-news2.p.rapidapi.com",
     },
   };
 
@@ -23,30 +23,30 @@ async function fetchDataFromRapidAPI() {
 
 function displayNewsCards(newsData) {
   // Clear the existing cards in the container
-  cardsContainer.innerHTML = '';
+  cardsContainer.innerHTML = "";
 
   // Loop through the news data and create a card for each news item
   newsData.forEach((newsItem) => {
     // Clone the template for each news item
-    const template = document.getElementById('template-news-card');
+    const template = document.getElementById("template-news-card");
     const card = template.content.cloneNode(true);
 
     // Update the content of the card with the news data
-    const cardImage = card.querySelector('#news-img');
-    const cardTitle = card.querySelector('#news-title');
-    const cardSource = card.querySelector('#news-source');
-    const cardDesc = card.querySelector('#news-desc');
+    const cardImage = card.querySelector("#news-img");
+    const cardTitle = card.querySelector("#news-title");
+    const cardSource = card.querySelector("#news-source");
+    const cardDesc = card.querySelector("#news-desc");
 
-    // Redirect news 
-    card.firstElementChild.addEventListener('click', () => {
-      window.open(linkUrl, '_blank'); // Open the link in a new tab when the card is clicked
+    // Redirect news
+    card.firstElementChild.addEventListener("click", () => {
+      window.open(linkUrl, "_blank"); // Open the link in a new tab when the card is clicked
     });
 
     // Set the image URL from the API response
-    const imageUrl = newsItem.image || 'https://via.placeholder.com/400x200'; // Use fallback image if image URL is not provided
+    const imageUrl = newsItem.image || "https://via.placeholder.com/400x200"; // Use fallback image if image URL is not provided
 
     // Set the link URL from the API response
-    const linkUrl = newsItem.link || '#'; // Use a placeholder link if link URL is not provided
+    const linkUrl = newsItem.link || "#"; // Use a placeholder link if link URL is not provided
 
     cardImage.src = imageUrl;
     cardTitle.textContent = newsItem.title;
@@ -54,8 +54,8 @@ function displayNewsCards(newsData) {
     cardDesc.textContent = newsItem.description;
 
     // Set the link for the entire card
-    card.addEventListener('click', () => {
-      window.open(linkUrl, '_blank'); // Open the link in a new tab when the card is clicked
+    card.addEventListener("click", () => {
+      window.open(linkUrl, "_blank"); // Open the link in a new tab when the card is clicked
     });
 
     // Append the card to the cards container
